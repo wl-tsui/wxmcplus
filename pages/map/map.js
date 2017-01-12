d62ebb97c02994515b66d2acbe29ea924abdfb07
wl-tsui <wltsui@moyogame.com>
2017-01-12 15:53:14 +0800

    v3

--- pages/map/map.js
+++ pages/map/map.js
@@ -4,29 +4,29 @@
 
 
 var util = require('../../utils/util.js')
+var pois = [];
 var app = getApp()
 Page({
   data: {
     markers: [],
+    poi: {},
+    lat: '',
+    lng: '',
     controls: [{
-      id: 1,
+      id: 2,
       iconPath: './images/gis.png',
       position: {
-        left: 0,
+        left: 5,
         top: 600 - 30,
-        width: 30,
-        height: 30
       },
       clickable: true
     },
     {
-      id: 2,
+      id: 1,
       iconPath: './images/center.png',
       position: {
         left: 200,
         top: 300 - 30,
-        width: 30,
-        height: 30
       },
       clickable: false
     },
@@ -48,6 +48,10 @@ Page({
       this.mapCtx.getCenterLocation({
         success: function (res) {
           console.log("****now center,  lat: ", res.latitude, ", lon: ", res.longitude)
+          that.setData({
+            lat: res.latitude,
+            lng: res.longitude
+          });
           that.getPois(res.latitude, res.longitude)
           // this.mapCtx.moveToLocation()
         }
@@ -55,16 +59,39 @@ Page({
     }
   },
   markertap(e) {
-    console.log(e.markerId)
+    var that = this;
+    that.setData({
+      poi:{
+        name: that.pois[e.markerId].name,
+        level: that.pois[e.markerId].level
+      }
+    })
   },
   controltap(e) {
     const that = this;
-    if(e.controlId==3){
-      wx.chooseLocation({success: function(res){
-        console.log("chooseLocation,  lat: ", res.latitude, ", lon: ", res.longitude)
-        that.getPois(res.latitude, res.longitude)
-      }
-    })
+    if (e.controlId == 3) {
+      wx.chooseLocation({
+        success: function (res) {
+          console.log("chooseLocation,  lat: ", res.latitude, ", lon: ", res.longitude)
+          that.setData({
+            lat: res.latitude,
+            lng: res.longitude
+          });
+          that.getPois(res.latitude, res.longitude)
+        }
+      })
+    } else if (e.controlId == 2) {
+      wx.getLocation({
+        type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+        success: function (res) {
+          console.log("location,  lat: ", res.latitude, ", lon: ", res.longitude)
+          that.setData({
+            lat: res.latitude,
+            lng: res.longitude
+          });
+          that.getPois(res.latitude, res.longitude)
+        }
+      })
     }
   },
   bindViewTap(e) {
@@ -82,26 +109,44 @@ Page({
       type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
       success: function (res) {
         console.log("location,  lat: ", res.latitude, ", lon: ", res.longitude)
+        that.setData({
+          lat: res.latitude,
+          lng: res.longitude
+        });
         that.getPois(res.latitude, res.longitude)
       }
     })
   },
+
   getPois(lat, lon) {
     const that = this;
+    var displayType = "pois"; //other
     wx.request({
       url: "http://hbtest.dworld.cn/Map/getMapInfo",
-      data: '{ "lat": ' + lat + ', "lon": ' + lon + ', "type": "osm" }',
+      data: '{ "lat": ' + lat + ', "lon": ' + lon + ', "type": "osm", "gType": "'+displayType+'" }',
       header: { 'content-type': 'application/x-www-form-urlencoded' },
       method: 'POST',
       success: res => {
-        // console.log(res.data.main.pois)
+        if( displayType == "other" ){
+          that.pois = res.data.main.allthings;
+        }else{
+          that.pois = res.data.main.pois;
+        }
+        // console.log(that.pois)
         var ar = new Array();
-        for (var k in res.data.main.pois) {
+        for (var k in that.pois) {
           var _data = new Object();
-          _data.iconPath = "./images/" + res.data.main.pois[k].race + ".png"
-          _data.latitude = res.data.main.pois[k].latitude
-          _data.longitude = res.data.main.pois[k].longitude
-          _data.title = res.data.main.pois[k].name
+           if( displayType == "other" ){
+            if( that.pois[k].type == 1 )
+              _data.iconPath = "./images/money.png"
+            else
+              _data.iconPath = "./images/hero.png"
+          }else{
+            _data.iconPath = "./images/" + that.pois[k].race + ".png"
+            _data.title = that.pois[k].name
+          }
+          _data.latitude = that.pois[k].latitude
+          _data.longitude = that.pois[k].longitude
           _data.id = k
           // _data.width = 40
           // _data.height = 51
